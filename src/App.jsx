f1a06511f110e5fc614a961a71b388d440b32958
portfolio-site <engineer.mohammadibrahim@gmail.com>
2024-08-28 09:36:56 +0500

    JS Questions

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JSQuestions from "./JSInterviewQuestions/JSQuestions";
+
+function App() {
+  return (
+    <>
+      <JSQuestions />
+    </>
+  );
+}
+
+export default App;
